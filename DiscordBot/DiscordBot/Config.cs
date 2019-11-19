c2d29f23c45391cea33eb3d3c25bad21497405db
Piotr Musielski <piotrmusielski@mail.com>
2019-11-19 23:20:20 +0100

    v0.1
    Fully set up,
    no commands

---
+++ DiscordBot/DiscordBot/Config.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DiscordBot
+{
+    class Config
+    {
+        private const string ConfigFolder = "Resources";
+        private const string ConfigFile = "config.json";
+
+        public static BotConfig bot;
+
+        static Config()
+        {
+            if (!Directory.Exists(ConfigFolder))
+                Directory.CreateDirectory(ConfigFolder);
+
+            if (!File.Exists(ConfigFolder + "/" + ConfigFile))
+            {
+                bot = new BotConfig();
+                string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+                File.WriteAllText(ConfigFolder + "/" + ConfigFile, json);
+            }
+            else
+            {
+                string json = File.ReadAllText((ConfigFolder + "/" + ConfigFile));
+                bot = JsonConvert.DeserializeObject<BotConfig>(json);
+                
+            }
+
+        }
+
+        public struct BotConfig
+        {
+            public string token;
+            public char cmdPrefix;
+        }
+    }
+
+
+}
---
+++ DiscordBot/DiscordBot/Modules/Misc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class Misc :ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+
+            var embed = new EmbedBuilder();
+
+            embed.WithTitle("Echoed message");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false,embed.Build());
+
+        }
+
+
+
+    }
+}
