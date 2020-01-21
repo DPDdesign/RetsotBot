487d5bd2478ba03525321e5e37d79fecab57a406
Boydardi <60153251+Boydardi@users.noreply.github.com>
2020-01-21 16:42:56 -0500

    Update Misc.cs
    Spacing and Alignment Update to Several Sections

--- DiscordBot/DiscordBot/Modules/Misc.cs
+++ DiscordBot/DiscordBot/Modules/Misc.cs
@@ -158,7 +158,7 @@ namespace DiscordBot.Modules
             await Context.Channel.SendMessageAsync("DX");
         }
 
-                [Command("WriteToSpreadsheet")]
+        [Command("WriteToSpreadsheet")]
         public async Task WriteToSpreadsheet([Remainder]string message)
         {
             string[] options = message.Split('|');   
@@ -373,25 +373,24 @@ namespace DiscordBot.Modules
             Utilities.playerscount ++;
 
             var embed = new EmbedBuilder();
-           
-      
-
-            if(!Utilities.checkedin.Contains(DCN)){
-            Utilities.DiscordNames += DCN + "\n";
-            Utilities.GameNames += IGN + "\n";
-            embed.WithTitle("Welcome!");
-            embed.WithDescription(Context.User.Mention + " you have succesfully checked in as: \n" + IGN);
-            embed.WithColor(new Color(0, 255, 0));
-            embed.WithFooter(footer => footer.Text = "Current players checked in: " + Utilities.playerscount + "/ 128");
-             embed.WithThumbnailUrl(Context.User.GetAvatarUrl());
-            //embed.WithUrl("https://example.com");
-            embed.WithCurrentTimestamp();
-            Utilities.checkedin.Add(DCN);
+            if(!Utilities.checkedin.Contains(DCN))
+            {
+                Utilities.DiscordNames += DCN + "\n";
+                Utilities.GameNames += IGN + "\n";
+                embed.WithTitle("Welcome!");
+                embed.WithDescription(Context.User.Mention + " you have succesfully checked in as: \n" + IGN);
+                embed.WithColor(new Color(0, 255, 0));
+                embed.WithFooter(footer => footer.Text = "Current players checked in: " + Utilities.playerscount + "/ 128");
+                 embed.WithThumbnailUrl(Context.User.GetAvatarUrl());
+                //embed.WithUrl("https://example.com");
+                embed.WithCurrentTimestamp();
+                Utilities.checkedin.Add(DCN);
             }
 
-            else{
-            embed.WithTitle("WARNING!");
-            embed.WithDescription("You have already checked in!");
+            else
+            {
+                embed.WithTitle("WARNING!");
+                embed.WithDescription("You have already checked in!");
             }
 
            await Context.Channel.SendMessageAsync(null, false, embed.Build());
@@ -401,82 +400,67 @@ namespace DiscordBot.Modules
         public async Task IGN()
         {          
             var embed = new EmbedBuilder();
-
             embed.WithTitle("List of Game Names:");
             embed.WithDescription(Utilities.DiscordNames);
             await Context.Channel.SendMessageAsync("", false, embed.Build());
-        } */
-        /*
-         * 
-         *         [Command("echo")]
-                public async Task Echo([Remainder]string message)
-                {
+        } 
+        
+          
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Echoed message");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+            await Context.Channel.SendMessageAsync("", false,embed.Build());
+        }
+        
+        public List<string> Roles = new List<string>();
 
-                    var embed = new EmbedBuilder();
+        [Command("GenerateTeam")]
+        public async Task JoinVoiceChannel([Remainder]string message)
+        {
+            Roles.Add("Top");
+            Roles.Add("Mid");
+            Roles.Add("Jungle");
+            Roles.Add("ADC");
+            Roles.Add("Support");
 
-                    embed.WithTitle("Echoed message");
-                    embed.WithDescription(message);
-                    embed.WithColor(new Color(0, 255, 0));
+            Random r = new Random();
+            int i = 0;
+            string write = "";
 
-                    await Context.Channel.SendMessageAsync("", false,embed.Build());
+            var embed = new EmbedBuilder();
 
-                }
-                public List<string> Roles = new List<string>();
-         * 
-         * 
-         * 
-                [Command("GenerateTeam")]
-                public async Task JoinVoiceChannel([Remainder]string message)
-                {
+            int n = Roles.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = r.Next(n + 1);
+                var value = Roles[k];
+                Roles[k] = Roles[n];
+                Roles[n] = value;
+            }
 
-                    Roles.Add("Top");
-                    Roles.Add("Mid");
-                    Roles.Add("Jungle");
-                    Roles.Add("ADC");
-                    Roles.Add("Support");
+            await Context.Channel.SendMessageAsync(Context.User.Username + "Have randomized a team named: " + message);
+            var VoiceUsers = Context.Guild.GetVoiceChannel(619552205892091927).Users;
+            foreach (var v in VoiceUsers)
+            {
+                write += (v.Mention  +  "   - "  + Roles[i] + "\n");
+                i++;
+            }
 
-                    Random r = new Random();
-                    int i = 0;
-                    string write = "";
+            embed.WithTitle("Team " +  "DD: " + Context.Message.Author + message);
+            embed.WithDescription(write);
+            embed.WithColor(new Color(0, 255, 0));
+            //embed.WithFooter(footer => footer.Text = "I am a footer.");
+            //embed.WithUrl("https://example.com");
+            embed.WithCurrentTimestamp();
 
-                                var embed = new EmbedBuilder();
-
-
-                        int n = Roles.Count;
-                        while (n > 1)
-                        {
-                            n--;
-                            int k = r.Next(n + 1);
-                            var value = Roles[k];
-                            Roles[k] = Roles[n];
-                            Roles[n] = value;
-                        }
-
-
-
-
-                    //await Context.Channel.SendMessageAsync(Context.User.Username + "Have randomized a team named: " + message);
-                    var VoiceUsers = Context.Guild.GetVoiceChannel(619552205892091927).Users;
-                    foreach (var v in VoiceUsers)
-                    {
-                        write += (v.Mention  +  "   - "  + Roles[i] + "\n");
-
-                        i++;
-                    }
-
-
-
-                    embed.WithTitle("Team " +  "DD: " + Context.Message.Author + message);
-                    embed.WithDescription(write);
-                    embed.WithColor(new Color(0, 255, 0));
-                    //embed.WithFooter(footer => footer.Text = "I am a footer.");
-                    //embed.WithUrl("https://example.com");
-                    embed.WithCurrentTimestamp();
-
-
-                    Roles.Clear();
-                   await Context.Channel.SendMessageAsync(Context.User.Username + " Created:", false, embed.Build());
-                }
+            Roles.Clear();
+            await Context.Channel.SendMessageAsync(Context.User.Username + " Created:", false, embed.Build());
+        }
         */
         // embed.WithColor(new Color(255, 255, 0));
         // embed.WithThumbnailUrl(Context.User.GetAvatarUrl());
